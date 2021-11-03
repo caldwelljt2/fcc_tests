456981436fa74f1a5918dc2490ffee7aaffed16b
caldwelljt2 <caldwelljt@airboxinc.com>
2021-11-03 15:38:34 -0400

    Added sorted Union

---
+++ sortedUnion.js
@@ -0,0 +1,23 @@
+function uniteUnique(...arr) {
+    console.log('Question: \n',arr)
+    let newArray = arr[0]
+    
+    for (let item in arr) {
+      for (let num in arr[item]) {
+           if (item > 0) {
+              
+              if (newArray.some( (num2) => {return (num2 == arr[item][num])})) {
+              } else {
+                newArray.push(arr[item][num])
+              }
+            }
+           }  
+    
+    }
+      console.log('Answer: \n', newArray)
+      return newArray;
+    }
+     
+    uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+    uniteUnique([1, 2, 3], [5, 2, 1]);
+    uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]);
