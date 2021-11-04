c511539f36a10892d93c96fcc80dd208fb893720
caldwelljt2 <caldwelljt@airboxinc.com>
2021-11-04 14:27:03 -0400

    fixed 2 character bug and corrected for use case (sentances with punctuation)

--- palindromCheck.js
+++ palindromCheck.js
@@ -1,9 +1,11 @@
-function palindrome(str) {
-    if (str.length <= 2) {
+  function palindrome(str) {
+    let word = str.replace(/[^a-zA-Z0-9]+/g,"").toLowerCase()   // didn't relize the test wanted to test sentances and remove all punctuation
+    console.log(word)
+    if (word.length <= 2 && word[0] == word[word.length-1]) {  // forgot to take into account 2 character missmatched cases
       return true
-    } else if (str[0] == str[str.length-1]) {
-      console.log(str,str.slice(1, -1))
-      return palindrome(str.slice(1, -1))  // forgot to RETURN this for way tooo long. how else would it "capture" the base's true from above?
+    } else if (word[0] == word[word.length-1]) {
+      console.log(word,word.slice(1, -1))
+      return palindrome(word.slice(1, -1)) // forgot to RETURN this for way tooo long. how else would it "capture" the base's true from above?
       // return true
     } else {
       return false;
@@ -11,5 +13,8 @@ function palindrome(str) {
   // return true
   }
   
-  console.log(palindrome("abcdefgfedcba"));
+  console.log(palindrome("almostomla"));
   
+  // console.log("_eye".replace(/\W/g,""))
+  // console.log("almostomla".replace(/[^a-zA-Z]+/g,""))
+  // console.log("moadsf".slice(1, -1))
